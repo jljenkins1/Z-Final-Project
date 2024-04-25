fe2086933bf5226e48e280faf2b2161a1d528585
jljcb <jljcb@DellXPS13>
2024-04-24 20:57:56 -0500

    added enum Types, added constants and a static method to determine move effectiveness.

---
+++ src/test/Type.java
@@ -0,0 +1,10 @@
+package test;
+
+public enum Type {
+	FIRE, WATER, GRASS, BUG, DRAGON, ELECTRIC, FIGHTING, FLYING, GHOST, GROUND, ICE, NORMAL, POISON, PSYCHIC, DARK;
+	
+	public static double getEffectiveness(Type attackType, defenseType) {
+		//implement logic
+		return 1.0; //placeholder
+	}
+}
