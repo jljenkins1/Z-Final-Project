0a605fc9eeca70a31186969d47e323410963c60c
jljcb <jljcb@DellXPS13>
2024-04-24 22:30:20 -0500

    added main implementation. added getInstance for players to get their pokemon and a method to intialize the battle

--- src/test/main.java
+++ src/test/main.java
@@ -3,7 +3,16 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Pokedex pokedex = Pokedex.getInstance();
+		Player playerOne = new Player("Josh");
+		Player playerTwo = new Player("Ethan");
+		
+		playerOne.addPokemonsToTeam(pokedex.getAllPokemon().get(0));
+		playerTwo.addPokemonsToTeam(pokedex.getAllPokemon().get(1));
+		
+		Battle battle = new Battle(playerOne, playerTwo);
+		battle.startBattle(); //Start the battle
+		
 
 	}
 
