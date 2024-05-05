5e1ba0601253050d9a78d5612e6a853bbdc2fbae
ecoles333 <156868624+ecoles333@users.noreply.github.com>
2024-05-05 13:28:56 -0500

    Update Pokedex.java
    added in jar compile for .json

--- src/test/Pokedex.java
+++ src/test/Pokedex.java
@@ -1,34 +1,41 @@
 package test;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.List;
 
-public class Pokedex {
-    private static final String filePath = "src/test/pokemon.json";
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-    // Method to display all Pokemon and their stats from the JSON file
+public class Pokedex {
+    private static final String FILE_PATH = "test/pokemon.json";
+
+    // Method to display all Pokemon and their stats from the JSON file (complies into the .jar file itself as well)
     public static void displayAllPokemon() {
         System.out.println("Complete Pokedex:");
-    	try {
+        try {
             Gson gson = new Gson();
-            FileReader reader = new FileReader(filePath);
-            Type pokemonListType = new TypeToken<List<Pokemon>>() {}.getType();
-            List<Pokemon> pokemons = gson.fromJson(reader, pokemonListType);
-            if (pokemons != null) {
-                for (Pokemon pokemon : pokemons) {
-                    System.out.println(pokemon.getName() + " - Type: " + pokemon.getPType() +
-                            ", Max HP: " + pokemon.getMaxHP() + ", Attack: " + pokemon.getAttack() +
-                            ", Defense: " + pokemon.getDefense() + ", Special Attack: " + pokemon.getSpecialAttack() +
-                            ", Special Defense: " + pokemon.getSpecialDefense());
+            InputStream inputStream = Pokedex.class.getClassLoader().getResourceAsStream(FILE_PATH);
+            if (inputStream != null) {
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                Type pokemonListType = new TypeToken<List<Pokemon>>() {}.getType();
+                List<Pokemon> pokemons = gson.fromJson(reader, pokemonListType);
+                if (pokemons != null) {
+                    for (Pokemon pokemon : pokemons) {
+                        System.out.println(pokemon.getName() + " - Type: " + pokemon.getPType() +
+                                ", Max HP: " + pokemon.getMaxHP() + ", Attack: " + pokemon.getAttack() +
+                                ", Defense: " + pokemon.getDefense() + ", Special Attack: " + pokemon.getSpecialAttack() +
+                                ", Special Defense: " + pokemon.getSpecialDefense());
+                    }
+                } else {
+                    System.out.println("No Pokémon found in the Pokedex.");
                 }
             } else {
-                System.out.println("No Pokémon found in the Pokedex.");
+                System.out.println("Pokemon JSON file not found.");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
